2d182735361d0ce30e0236d15d59ed8bb014088e
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-11-18 06:09:21 +0300

    Done Web Services with URLSession.

---
+++ WebServices/Model/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Photo.swift
+//  WebServices
+//
+//  Created by Mobark Alseif on 12/04/1443 AH.
+//
+
+import Foundation
+
+struct Book: Codable {
+
+    // MARK: - Properties
+    let title: String
+    let author: String
+
+}
